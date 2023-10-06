b5f1ec47a2a454572a2aad214800375785930d87
Erasmo Costa <erasmo.programar@gmail.com>
2023-10-06 11:17:37 -0300

    feat(MaquinaTroco): add note amount and exit condition

--- Projeto/MaquinaTroco.py
+++ Projeto/MaquinaTroco.py
@@ -1,5 +1,6 @@
 ## Projeto: M A Q U I N A   D E   T R O C O
 ## Autor: Erasmo Costa
+import random
 
 def validaNumeroRealPositivo(entrada):
     numerosValidos = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",",", "."]
@@ -17,12 +18,13 @@ def validaNumeroRealPositivo(entrada):
 
 def recebeEntradaUsuario(complemento):
     checkEntrada = False
-
+    entrada = input(f"\nInforme o valor total {complemento}: R$")
+    if entrada == 'sair' or entrada == 'SAIR':
+        return -1
     while not checkEntrada:
-        entrada = input(f"Informe o valor total {complemento}: R$")
         checkEntrada = validaNumeroRealPositivo(entrada)
         if not checkEntrada:
-            print(f"Valor inválido! Infome um valor válido.")
+            print(f"\nValor inválido! Infome um valor válido.")
     if "," in entrada:
         entrada = entrada.replace(",", ".")
     entrada = float(entrada)
@@ -38,36 +40,68 @@ def formataTipoCedula(cedula,qtdCedula):
             return "moedas"
         return "moeda"
 
+def formataBRL(valor):
+    return f"R${valor:.2f}".replace(".", ",")
+
+def calculaTrocoDisponivel(cedulas, estoqueCedulas):
+    trocoDisponivel = 0
+    for i in range(len(cedulas)):
+        trocoDisponivel += cedulas[i] * estoqueCedulas[i]
+    return trocoDisponivel
 ##
-valorConta = recebeEntradaUsuario("da conta")
+cedulas = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01]
+estoqueCedulas = [0] * len(cedulas)
+trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
 
-if valorConta == 0:
-    print("A conta não possui valor. Não é necessário troco.")
-else:
-    qtdCedula = 0
-    cedulas = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01]
-
-    valorPagamento = recebeEntradaUsuario("do pagemento")
-    auxValorConta = valorPagamento - valorConta
-
-    if auxValorConta < 0:
-        print("Valor do pagamento insuficiente!")
-    elif auxValorConta == 0:
-        print("Valor do pagamento igual ao valor da conta. Não é necessário troco!")
+for i in range(len(estoqueCedulas)):
+    if cedulas[i] > 1:
+        estoqueCedulas[i] = random.randint(0, 10)
     else:
-        trocoFormatado = f"R${valorPagamento - valorConta:.2f}".replace(".",",")
-        print(f"O troco é {trocoFormatado}. A melhor forma de pagar o troco é: ")
-        for cedula in cedulas:
-            while auxValorConta >= cedula:
-                auxValorConta -= cedula
-                qtdCedula += 1
-            nomeCedula = formataTipoCedula(cedula, qtdCedula)
-            if qtdCedula > 0:
-                if cedula < 1:
-                    if cedula == 0.01:
-                        print(f"{qtdCedula} {nomeCedula} de {cedula * 100:.0f} centavo.")
-                    else:
-                        print(f"{qtdCedula} {nomeCedula} de {cedula * 100:.0f} centavos.")
-                else:
-                    print(f"{qtdCedula} {nomeCedula} de R${cedula},00.")
-            qtdCedula = 0
+        estoqueCedulas[i] = random.randint(0, 20)
+
+print("MÁQUINA DE TROCO - digite 'sair' para sair")
+
+while True:
+    valorConta = recebeEntradaUsuario("da conta")
+    if valorConta < 0:
+        break
+    elif valorConta == 0:
+        print("A conta não possui valor. Não é necessário troco.\n")
+    else:
+        qtdCedula = 0
+        valorPagamento = recebeEntradaUsuario("do pagemento")
+
+        if valorPagamento < 0:
+            break
+
+        troco = valorPagamento - valorConta
+
+        if troco < 0:
+            print("\nValor do pagamento insuficiente!\n")
+        elif troco == 0:
+            print("\nValor do pagamento igual ao valor da conta. Não é necessário troco!")
+        else:
+            print(f"\nO troco é {formataBRL(troco)}.")
+
+            if trocoDisponivel < troco:
+                print(f"\nNão há troco o suficiente no caixa! Troco total em caixa: R${formataBRL(trocoDisponivel)}.")
+            else:
+                print(f"A melhor forma de pagar o troco é: ")
+                for i in range(len(cedulas)):
+                    while troco >= cedulas[i] and estoqueCedulas[i]:
+                        troco -= cedulas[i]
+                        qtdCedula += 1
+                        estoqueCedulas[i] -= 1
+                    nomeCedula = formataTipoCedula(cedulas[i], qtdCedula)
+                    if qtdCedula > 0:
+                        if cedulas[i] < 1:
+                            if cedulas[i] == 0.01:
+                                print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavo.")
+                            else:
+                                print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavos.")
+                        else:
+                            print(f"{qtdCedula} {nomeCedula} de R${cedulas[i]},00.")
+                    qtdCedula = 0
+
+            trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
+            print(f"\n--Troco total em caixa: R${formataBRL(trocoDisponivel)}.--")
