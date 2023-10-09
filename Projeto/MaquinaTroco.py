3abdd968891489c0ea733ac32f0c78f2dccbc0d3
Erasmo Costa <erasmo.programar@gmail.com>
2023-10-09 14:12:14 -0300

    refactor(estoqueCedulas): chenge estoqueCedulas print

--- Projeto/MaquinaTroco.py
+++ Projeto/MaquinaTroco.py
@@ -19,16 +19,13 @@ def validaNumeroRealPositivo(entrada):
 def recebeEntradaUsuario(complemento):
     checkEntrada = False
     entrada = input(f"\nInforme o valor total {complemento}: R$")
-    if entrada == 'sair' or entrada == 'SAIR':
-        return -1
     while not checkEntrada:
         checkEntrada = validaNumeroRealPositivo(entrada)
         if not checkEntrada:
             print(f"\nValor inválido! Infome um valor válido.")
     if "," in entrada:
         entrada = entrada.replace(",", ".")
-    entrada = float(entrada)
-    return entrada
+    return float(entrada)
 
 def formataTipoCedula(cedula,qtdCedula):
     if cedula > 1:
@@ -51,7 +48,6 @@ def calculaTrocoDisponivel(cedulas, estoqueCedulas):
 ##
 cedulas = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01]
 estoqueCedulas = [0] * len(cedulas)
-trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
 
 for i in range(len(estoqueCedulas)):
     if cedulas[i] > 1:
@@ -59,49 +55,70 @@ for i in range(len(estoqueCedulas)):
     else:
         estoqueCedulas[i] = random.randint(0, 20)
 
-print("MÁQUINA DE TROCO - digite 'sair' para sair")
+trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
 
-while True:
-    valorConta = recebeEntradaUsuario("da conta")
-    if valorConta < 0:
-        break
-    elif valorConta == 0:
-        print("A conta não possui valor. Não é necessário troco.\n")
-    else:
+print("MÁQUINA DE TROCO")
+print("---------------------------------------------------------------------------------------------------------------")
+print("Troco disponível em caixa:")
+print("Notas:\n", end = "")
+for cedula in cedulas:
+    print(f"{cedula}".rjust(4), end =" | ")
+print()
+print("Qtd. Estoque:")
+for estoque in estoqueCedulas:
+    print(f"{estoque}".rjust(4), end =" | ")
+print(f"\n\nTroco total em caixa: R${formataBRL(trocoDisponivel)}.")
+print("---------------------------------------------------------------------------------------------------------------")
+
+valorConta = recebeEntradaUsuario("da conta")
+
+if valorConta == 0:
+    print("A conta não possui valor. Não é necessário troco.\n")
+else:
+    troco = -1
+    while troco < 0:
         qtdCedula = 0
-        valorPagamento = recebeEntradaUsuario("do pagemento")
-
-        if valorPagamento < 0:
-            break
-
+        valorPagamento = recebeEntradaUsuario("do pagamento")
         troco = valorPagamento - valorConta
+        print("\n--VALOR DO PAGAMENTO INSUFICIENTE!--")
 
-        if troco < 0:
-            print("\nValor do pagamento insuficiente!\n")
-        elif troco == 0:
-            print("\nValor do pagamento igual ao valor da conta. Não é necessário troco!")
+    if troco == 0:
+        print("\nValor do pagamento igual ao valor da conta. Não é necessário troco!")
+    else:
+        print(f"\nO troco é {formataBRL(troco)}.")
+
+        if trocoDisponivel < troco:
+            print(f"\nNão há troco o suficiente no caixa! Troco total em caixa: R${formataBRL(trocoDisponivel)}.")
         else:
-            print(f"\nO troco é {formataBRL(troco)}.")
-
-            if trocoDisponivel < troco:
-                print(f"\nNão há troco o suficiente no caixa! Troco total em caixa: R${formataBRL(trocoDisponivel)}.")
-            else:
-                print(f"A melhor forma de pagar o troco é: ")
-                for i in range(len(cedulas)):
-                    while troco >= cedulas[i] and estoqueCedulas[i]:
-                        troco -= cedulas[i]
-                        qtdCedula += 1
-                        estoqueCedulas[i] -= 1
-                    nomeCedula = formataTipoCedula(cedulas[i], qtdCedula)
-                    if qtdCedula > 0:
-                        if cedulas[i] < 1:
-                            if cedulas[i] == 0.01:
-                                print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavo.")
-                            else:
-                                print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavos.")
+            print(f"A melhor forma de pagar o troco é: ")
+            for i in range(len(cedulas)):
+                while troco >= cedulas[i] and estoqueCedulas[i]:
+                    troco -= cedulas[i]
+                    qtdCedula += 1
+                    estoqueCedulas[i] -= 1
+                nomeCedula = formataTipoCedula(cedulas[i], qtdCedula)
+                if qtdCedula > 0:
+                    if cedulas[i] < 1:
+                        if cedulas[i] == 0.01:
+                            print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavo.")
                         else:
-                            print(f"{qtdCedula} {nomeCedula} de R${cedulas[i]},00.")
-                    qtdCedula = 0
+                            print(f"{qtdCedula} {nomeCedula} de {cedulas[i] * 100:.0f} centavos.")
+                    else:
+                        print(f"{qtdCedula} {nomeCedula} de R${cedulas[i]},00.")
+                qtdCedula = 0
 
-            trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
-            print(f"\n--Troco total em caixa: R${formataBRL(trocoDisponivel)}.--")
+        trocoDisponivel = calculaTrocoDisponivel(cedulas, estoqueCedulas)
+
+        print(
+            "\n---------------------------------------------------------------------------------------------------------------")
+        print("Troco disponível em caixa:")
+        print("Notas:\n", end="")
+        for cedula in cedulas:
+            print(f"{cedula}".rjust(4), end=" | ")
+        print()
+        print("Qtd. Estoque:")
+        for estoque in estoqueCedulas:
+            print(f"{estoque}".rjust(4), end=" | ")
+        print(f"\n\nTroco total em caixa: R${formataBRL(trocoDisponivel)}.")
+        print(
+            "---------------------------------------------------------------------------------------------------------------")
